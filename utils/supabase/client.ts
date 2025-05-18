2127d378cd3e2c4fb692ae7bbd5906b57ced2aea
Rojan5656 <www.rojan2020@gmail.com>
2025-05-18 13:21:33 +0545

    removed Url and Key

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://rrdinvpcklvdgrgytces.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyZGludnBja2x2ZGdyZ3l0Y2VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1NDc4MTYsImV4cCI6MjA2MzEyMzgxNn0._mUInSPCmy2jbW3zXMwUl3Vvnj3QGsY2P4yWhsXOJZI"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
